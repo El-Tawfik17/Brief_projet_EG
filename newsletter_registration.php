b2a0b90bc1198414db588d55dfb81114df409516
El-Tawfik <kifwat17@gmail.com>
2022-07-01 17:57:09 +0100

    update blog

--- newsletter_registration.php
+++ newsletter_registration.php
@@ -12,6 +12,20 @@ $stmt = mysqli_stmt_init($conn);
  );
  
  mysqli_stmt_execute($stmt);
- echo '<h1 class=\"bg-success text-sucess\">votre inscrption est effectuée</h1>';
+ echo '<h1 class="bg-success text-sucess">votre inscrption est effectuée</h1>';
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="stylesheet" rel="css/bootstrap.min.css">
+    <title>soumission</title>
+</head>
+<body>
+  <a href="index.html" class="btn btn-primary"> Retourner à l'acceuil</a>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
